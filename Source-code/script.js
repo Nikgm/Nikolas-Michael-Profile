6fede6bfbabea1681c5af04bae2807faa6d1b6e5
Nikgm <39501813+Nikgm@users.noreply.github.com>
2021-05-06 15:15:36 -0400

    Add files via upload

--- Source-code/script.js
+++ Source-code/script.js
@@ -1,3 +1,16 @@
+/*MENU BAR*/
+const windowSize = matchMedia('(max-width:768px)')
+windowSize.addEventListener('change',({matches})=>{
+    if(matches != true){
+        menuButton.style.display = 'none'
+        menuList.style.display = 'initial'
+        exitMenu.style.display = 'none'
+    }else{
+        menuButton.style.display = 'block'
+        menuList.style.display = 'none'
+        exitMenu.style.display = 'none'
+}})
+
 /*NAVIGATION BAR*/
 const mainPageContent = document.getElementById('main-page-content');
 const projectsContent = document.getElementById('projects-content');
@@ -73,5 +86,39 @@ pdfLink.addEventListener('click', ()=>{
     pdfText.innerHTML = '(This link does nothing :P)'
 })
 
+/*CONTACT FORM*/
+const subjectField = document.getElementById('subject-field');
+const maxLength = document.getElementById('max-length');
+const subjectMaxLength = document.getElementById('subject-max-length');
+const textArea = document.querySelector('textarea');
+const textMaxLength = document.getElementById('message-max-length');
+let subjectFieldCount = 0;
+let textAreaCount = 0;
 
+subjectField.addEventListener('keydown', ()=>{
+    subjectFieldCount = subjectField.value.length
+    if (subjectFieldCount >= 100){
+        maxLength.style.display = 'initial'
+        subjectMaxLength.style.display = 'initial'
+    }else if(textAreaCount >= 1000 && subjectFieldCount < 100){
+        maxLength.style.display = 'initial'
+        subjectMaxLength.style.display = 'none'
+    }else{
+        maxLength.style.display = 'none'
+        subjectMaxLength.style.display = 'none'
+    }
+})
 
+textArea.addEventListener('keydown',()=>{
+    textAreaCount = textArea.value.length
+    if (textAreaCount >= 1000){
+        maxLength.style.display = 'initial'
+        textMaxLength.style.display = 'initial'
+    }else if(textAreaCount < 1000 && subjectFieldCount >= 100){
+        maxLength.style.display = 'initial'
+        textMaxLength.style.display = 'none'
+    }else{
+        maxLength.style.display = 'none'
+        subjectMaxLength.style.display = 'none'
+    }
+})
